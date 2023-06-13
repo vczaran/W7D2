4400375ae33cbb6cf9da2dd9d8fe9d6a9be01f22
Viktoria Czaran <vczaran@gmail.com>
2023-06-13 14:47:03 -0400

    Completed users new view

--- MusicApp/app/controllers/application_controller.rb
+++ MusicApp/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ helper_method :current_user, :logged_in?
 #CHRRLLL
 
 def current_user
-    self.session_token = session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
 end
 
 
@@ -12,22 +12,23 @@ def logged_in?
 end
 
 def login!(user)
-
+    session[:session_token] = user.reset_session_token!
 end
 
 
-def logout
-
+def logout!
+    current_user.reset_session_token! if logged_in?
+    session[:session_token] = nil
+    @current_user = nil
 end
 
 def require_logged_in
-
+    redirect_to new_session_url unless logged_in?
 end
 
 
 def require_logged_out
-
-
+    redirect_to users_url if logged_in?
 end
 
 
--- MusicApp/app/controllers/users_controller.rb
+++ MusicApp/app/controllers/users_controller.rb
@@ -1,19 +1,34 @@
 class UsersController < ApplicationController
 
 def new
-
+    render :new
 end
 
 
 def create
+    @user = User.new(user_params)
+
+    if @user.save
+        login!(@user)
+        redirect_to user_url(@user)
+    else
+        flash.now[:errors] = @user.errors.full_messages
+        render :new
+    end
 
 end
 
 
 def show
     @user = User.find_by(params[:id])
-    redirect_to user_url(@user.id)    
+    render :show  
 end
 
+
+private
+
+def user_params
+    params.require(:user).permit(:email, :password)
+end
 
 end
